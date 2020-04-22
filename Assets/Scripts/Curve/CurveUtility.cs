af75513521a2be129c1db16ab74a416784492768
Xayko <mignotte.tom@gmail.com>
2020-04-22 13:08:15 +0200

    Animation for crouch

---
+++ Assets/Scripts/Curve/CurveUtility.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveUtility : MonoBehaviour
+{
+    public AnimationCurve axeX;
+    public AnimationCurve axeY;
+    public AnimationCurve axeZ;
+
+    private Vector3 curve = Vector3.zero;
+
+    private float movementTimerX = 0f;
+    private float movementTimerY = 0f;
+    private float movementTimerZ = 0f;
+
+    private float movementDurationX = 0f;
+    private float movementDurationY = 0f;
+    private float movementDurationZ = 0f;
+
+    public GameObject objectToAnimate;
+
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        movementTimerX += Time.deltaTime;
+        movementTimerY += Time.deltaTime;
+        movementTimerZ += Time.deltaTime;
+        curve = new Vector3(objectToAnimate.transform.parent.position.x + axeX.Evaluate(movementTimerX), objectToAnimate.transform.parent.position.y + axeY.Evaluate(movementTimerY), objectToAnimate.transform.parent.position.z + axeZ.Evaluate(movementTimerZ));
+        objectToAnimate.transform.position = curve;
+    }
+
+    private float ComputeAnimCurveDuration(AnimationCurve curve)
+    {
+        if (curve.keys.Length > 0)
+        {
+            float maxTime = curve.keys[0].time;
+            for (int i = 1; i < curve.keys.Length; i++)
+            {
+                // Je récupère la valeur la plus haute entre le dernier timing récupéré, et le timing de la keyframe 
+                maxTime = Mathf.Max(maxTime, curve.keys[i].time);
+            }
+            return maxTime;
+        }
+
+        return 0f;
+    }
+    public void BeginMovement()
+    {
+        movementTimerX = 0f;
+        movementDurationX = ComputeAnimCurveDuration(axeX);
+        movementTimerY = 0f;
+        movementDurationY = ComputeAnimCurveDuration(axeY);
+        movementTimerZ = 0f;
+        movementDurationZ = ComputeAnimCurveDuration(axeZ);
+    }
+}
--- Assets/Scripts/Player/Crouching.cs
+++ Assets/Scripts/Player/Crouching.cs
@@ -10,11 +10,19 @@ public class Crouching : MonoBehaviour
 
     private Player m_PlayerScript;
 
+    private CurveUtility m_CurveUtility;
+
+    public AnimationCurve crouching;
+
+    public AnimationCurve invCrouching;
+
     private void Start()
     {
         m_PlayerCollider = GetComponentInChildren<BoxCollider>();
         m_PlayerCamera = GetComponentInChildren<Camera>();
         m_PlayerScript = GetComponent<Player>();
+        m_CurveUtility = GetComponent<CurveUtility>();
+        m_CurveUtility.objectToAnimate = m_PlayerCamera.gameObject;
     }
     void Update()
     {
@@ -34,23 +42,29 @@ public class Crouching : MonoBehaviour
 
     private void ChangeColliderOnCrouching()
     {
-        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        if (Input.GetKeyDown(KeyCode.C))
         {
             m_PlayerCollider.center = new Vector3(0, -0.25f, 0);
             m_PlayerCollider.size = new Vector3(1, 0.5f, 1);
         }
-        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        if (Input.GetKeyUp(KeyCode.C))
         {
             m_PlayerCollider.center = new Vector3(0, 0, 0);
-            m_PlayerCollider.size = new Vector3(1, 1, 1);
+            m_PlayerCollider.size = new Vector3(1, 1.756f, 1);
         }
     }
 
     private void ChangePositionCameraOnCrouching()
     {
-        if (Input.GetKeyDown(KeyCode.LeftAlt))
-            m_PlayerCamera.transform.position = m_PlayerCamera.transform.parent.parent.position - new Vector3(0, 0.5f, 0);
-        if(Input.GetKeyUp(KeyCode.LeftAlt))
-            m_PlayerCamera.transform.position = m_PlayerCamera.transform.parent.parent.position + new Vector3(0, 0.5f, 0);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            m_CurveUtility.axeY = crouching;
+            m_CurveUtility.BeginMovement();
+        }
+        if(Input.GetKeyUp(KeyCode.C))
+        {
+            m_CurveUtility.axeY = invCrouching;
+            m_CurveUtility.BeginMovement();
+        }
     }
 }
