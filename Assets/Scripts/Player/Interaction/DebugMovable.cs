cf54efe393128adfd02d85decbbdbc23a20c4896
Achillenotpass <achille.pace@laposte.net>
2020-04-22 12:14:05 +0200

    Fixed bugs on player prefab. Changed max vertical rotation on camera. Fixed bugs on interaction with mobile objects. Added chair prefab.

---
+++ Assets/Scripts/Player/Interaction/DebugMovable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMovable : MonoBehaviour
+{
+    public Rigidbody thisgameobject;
+    public Vector3 velocity2;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity2 = thisgameobject.velocity;
+        
+    }
+}
