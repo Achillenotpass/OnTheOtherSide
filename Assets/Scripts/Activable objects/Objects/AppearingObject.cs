8db174bf464088d8d7fadd87cee18d55a3b7ea73
Xayko <mignotte.tom@gmail.com>
2020-04-23 14:58:03 +0200

    light interruptor and appearing object

---
+++ Assets/Scripts/Activable objects/Objects/AppearingObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearingObject : Activable
+{
+    public GameObject[] objectToAppear;
+
+    public override void Interaction()
+    {
+        for(int i = 0; i < objectToAppear.Length; i++)
+            objectToAppear[i].SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Activable objects/Objects/LightInterruptor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightInterruptor : Activable
+{
+    public GameObject lightToActivate;
+
+    private SpeLight m_LightScript;
+
+    private void Awake()
+    {
+        m_LightScript = lightToActivate.GetComponent<SpeLight>();
+    }
+
+    public override void Interaction()
+    {
+        if (m_LightScript.currentLightState == LightState.On)
+            m_LightScript.currentLightState = LightState.Off;
+        else if (m_LightScript.currentLightState == LightState.Off)
+            m_LightScript.currentLightState = LightState.On;
+    }
+
+}
