5ffe6f317c422e0adc6a02e3ed8e6ff451fadb12
Xayko <mignotte.tom@gmail.com>
2020-04-28 11:15:45 +0200

    Trigger and unlock

---
+++ Assets/Scripts/Activable objects/Objects/UnlockDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockDoor : Activable
+{
+    public Door[] doors;
+
+    [Header("Do you want to unlock the doors ?")]
+    public bool unlock = true;
+
+    public override void Interaction()
+    {
+        for (int i = 0; i < doors.Length; i++)
+        {
+            switch(unlock)
+            {
+                case true:
+                    doors[i].isLocked = false;
+                    break;
+                case false:
+                    doors[i].isLocked = true;
+                    break;
+            }
+        }
+    }
+}
