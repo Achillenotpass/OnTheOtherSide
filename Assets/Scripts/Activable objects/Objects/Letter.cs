4c966aa74a041c7484cc52794a2b3af6cc92fdd7
Xayko <mignotte.tom@gmail.com>
2020-04-29 14:29:11 +0200

    interaction with letter

---
+++ Assets/Scripts/Activable objects/Objects/Letter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : Activable
+{
+    public GameObject letter;
+    CapsuleCollider capsuleCollider;
+    MeshRenderer meshRenderer;
+    public GameObject player;
+    public Activable[] objectToActivateBefore;
+    public Activable[] objectToActivateAfter;
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    public override void Interaction()
+    {
+        letter.SetActive(true);
+        meshRenderer.enabled = false;
+        for(int i = 0; i < objectToActivateBefore.Length; i++)
+        {
+            objectToActivateBefore[i].Interaction();
+        }
+    }
+
+    private void Update()
+    {
+        if(letter.activeSelf)
+        {
+            if(Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                letter.SetActive(false);
+                meshRenderer.enabled = true;
+                transform.position = player.transform.position + player.transform.forward + player.transform.up/2;
+                for (int i = 0; i < objectToActivateAfter.Length; i++)
+                {
+                    objectToActivateAfter[i].Interaction();
+                }
+            }
+        }
+    }
+}
