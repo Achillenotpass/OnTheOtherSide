99a147a84286d78ff2cf5a5cdab950d26392b821
Achillenotpass <achille.pace@laposte.net>
2020-04-29 14:34:36 +0200

    Added textures. Changed sounds.

---
+++ Assets/Scripts/Activable objects/Objects/ChangementsMultiplesLumières.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangementsMultiplesLumières : Activable
+{
+    public List<SpeLight> lightScripts;
+
+    public LightState firstState;
+    public LightState secondState;
+
+    [Header("This array contains the time (in secondes) between the 2 states")]
+    public float timeBetweenLightStates;
+
+    public override void Interaction()
+    {
+        foreach (SpeLight light in lightScripts)
+        {
+            light.currentLightState = firstState;
+        }
+        Invoke("SecondState", timeBetweenLightStates);
+    }
+
+    private void SecondState()
+    {
+        foreach (SpeLight light in lightScripts)
+        {
+            light.currentLightState = secondState;
+        }
+    }
+}
