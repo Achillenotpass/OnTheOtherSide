ae9f91d2f9dab6f32c6013d436df29d3585d304d
Xayko <mignotte.tom@gmail.com>
2020-05-05 15:33:37 +0200

    le skellette et je j'ai pas finis

---
+++ Assets/Scripts/Activable objects/Objects/AssemblingPart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssemblingPart : MonoBehaviour
+{
+    public List<Activable> objectsToActivate = new List<Activable>();
+
+    public List<string> nameOfPart = new List<string>();
+
+    public int totalNumberOfParts = 1;
+
+    public int currentNumberOfParts = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Movable")
+        {
+            for(int i = 0; i < nameOfPart.Count; i++)
+            {
+                if(collision.gameObject.name == nameOfPart[i])
+                {
+                    currentNumberOfParts += 1;
+                    for (int z = 0; z < transform.childCount; z++)
+                    {
+                        if(transform.GetChild(z).name == collision.gameObject.name)
+                        {
+                            transform.GetChild(z).gameObject.SetActive(true);
+                        }
+                    }
+                    if (currentNumberOfParts >= totalNumberOfParts)
+                    {
+                        foreach (Activable toActivate in objectsToActivate)
+                        {
+                            toActivate.Interaction();
+                        }
+                    }
+                    collision.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnigmaObjects/Crank.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crank : Activable
+{
+    public List<Activable> objectsToActivate = new List<Activable>();
+
+    public float maxGauge = 5;
+
+    public float currentGauge = 0;
+
+    bool m_IsCranking = false;
+
+    public GameObject player;
+
+    private Player m_ScriptPlayer;
+
+    private FPSCamera m_ScriptCamera;
+
+    private Camera m_Camera;
+
+    public GameObject handle;
+
+    private void Start()
+    {
+        m_ScriptPlayer = player.GetComponent<Player>();
+        m_ScriptCamera = player.GetComponentInChildren<FPSCamera>();
+        m_Camera = player.GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        if(m_IsCranking)
+        {
+            Cranked();
+        }
+    }
+
+    public override void Interaction()
+    {
+        m_ScriptPlayer.enabled = !enabled;
+        //m_ScriptCamera.enabled = !enabled;
+        switch(m_Camera.fieldOfView)
+        {
+            case 60:
+                m_Camera.fieldOfView = 55;
+                break;
+            case 55:
+                m_Camera.fieldOfView = 60;
+                break;
+        }
+        handle.tag = "Movable";
+        m_IsCranking = true;
+    }
+
+    void Cranked()
+    {
+
+
+        /*if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (currentGauge >= maxGauge)
+            {
+                foreach (Activable toActivate in objectsToActivate)
+                {
+                    toActivate.Interaction();
+                }
+                gameObject.tag = "Untagged";
+                Debug.Log("aaa");
+            }
+            else
+            {
+                currentGauge += 1 * Time.deltaTime;
+                transform.Rotate(0, 0, 1);
+            }
+        }*/
+    }
+}
