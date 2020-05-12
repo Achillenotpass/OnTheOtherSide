ec8236a470f46c51583ea2e022e0a130bf1c946c
Xayko <mignotte.tom@gmail.com>
2020-05-12 12:46:50 +0200

    manivelle prefabs curve, static lightmap neon

---
+++ Assets/Scripts/FixeFPS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixeFPS : MonoBehaviour
+{
+    void Start()
+    {
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+    }
+}
