cb946bfffb68dd9e12811d6070e614e09abc463c
Achillenotpass <achille.pace@laposte.net>
2020-05-26 10:40:00 +0200

    Finished end cutscene of the first level

---
+++ Assets/Scripts/Activable objects/Objects/ActivateOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnStart : MonoBehaviour
+{
+    public List<Activable> toActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Activable toActivate in toActivate)
+        {
+            toActivate.Interaction();
+        }
+    }
+}
