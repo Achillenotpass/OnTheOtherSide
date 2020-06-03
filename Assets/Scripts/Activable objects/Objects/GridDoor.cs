c326f93fb0639c326ce6aa1958308182e7c3bac8
Achillenotpass <achille.pace@laposte.net>
2020-06-03 12:20:10 +0200

    Copied the school into the second level.

--- Assets/Scripts/Activable objects/Objects/GridDoor.cs
+++ Assets/Scripts/Activable objects/Objects/GridDoor.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class GridDoor : Activable
 {
+    public AudioClip downSound;
+    public AudioClip finalDownSound;
+    private AudioSource m_AudioSource;
+
+    private int m_InteractionNumber = 0;
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+
     public override void Interaction()
     {
         transform.Translate(0.0f, 0.0f, -1.2f);
+
+        if (m_InteractionNumber == 3)
+        {
+            m_AudioSource.PlayOneShot(finalDownSound);
+        }
+        else
+        {
+            m_InteractionNumber = m_InteractionNumber + 1;
+            m_AudioSource.PlayOneShot(downSound);
+        }
     }
 }
