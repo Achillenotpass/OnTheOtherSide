8189260edcdbec8a5af0a270c66ec139c4af0b1b
Xayko <mignotte.tom@gmail.com>
2020-06-08 14:45:15 +0200

    le bending mieux

---
+++ Assets/Scripts/Player/BendingCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BendingCollider : MonoBehaviour
+{
+    private Animator m_Animator;
+
+    private void Start()
+    {
+        m_Animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            Debug.Log("aaa");
+            if (m_Animator.GetBool("IsCrouching"))
+            {
+                m_Animator.SetBool("IsBendingLeftCrouching", false);
+                m_Animator.SetBool("IsBendingRightCrouching", false);
+            }
+            else
+            {
+                m_Animator.SetBool("IsBendingLeft", false);
+                m_Animator.SetBool("IsBendingRight", false);
+            }
+        }
+    }
+}
