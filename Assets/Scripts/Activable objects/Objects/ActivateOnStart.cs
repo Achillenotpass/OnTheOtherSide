7f9f9051208d228553bd56ccb090c9c5ac11579e
Achillenotpass <achille.pace@laposte.net>
2020-06-11 14:53:20 +0200

    Added videos for Logo and intro. Added death canvas.

--- Assets/Scripts/Activable objects/Objects/ActivateOnStart.cs
+++ Assets/Scripts/Activable objects/Objects/ActivateOnStart.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class ActivateOnStart : MonoBehaviour
 {
+    public bool delayedActivation = false;
+    public float timeBeforeDelayedActivation;
     public List<Activable> toActivate;
 
     // Start is called before the first frame update
     void Start()
+    {
+        if (!delayedActivation)
+        {
+            foreach (Activable toActivate in toActivate)
+            {
+                toActivate.Interaction();
+            }
+        }
+        else
+        {
+            Invoke("DelayedActivation", timeBeforeDelayedActivation);
+        }
+        
+    }
+
+    public void DelayedActivation()
     {
         foreach (Activable toActivate in toActivate)
         {
--- Assets/Scripts/Player/Death.cs
+++ Assets/Scripts/Player/Death.cs
@@ -7,6 +7,18 @@ public class Death : MonoBehaviour
     public GameObject respawnPoint;
 
     private bool m_IsDying = false;
+    public float deathTime;
+
+    //To disable
+    public GameObject canvas;
+    public GameObject deathCanvas;
+
+    private Player m_Player;
+    private Bending m_Bending;
+    private Crouching m_Crouching;
+    private FPSCamera m_FPSCamera;
+    private Interaction m_Interaction;
+
 
     private void Update()
     {
@@ -15,11 +27,33 @@ public class Death : MonoBehaviour
 
     public void Die()
     {
-        m_IsDying = true;
-        if (m_IsDying)
-        {
-            transform.position = respawnPoint.transform.position;
-            m_IsDying = false;
-        }
+        transform.position = respawnPoint.transform.position;
+
+        m_Player = FindObjectOfType<Player>();
+        m_Bending = FindObjectOfType<Bending>();
+        m_Crouching = FindObjectOfType<Crouching>();
+        m_FPSCamera = FindObjectOfType<FPSCamera>();
+        m_Interaction = FindObjectOfType<Interaction>();
+
+        canvas.SetActive(false);
+        deathCanvas.SetActive(true);
+        m_Player.enabled = false;
+        m_Bending.enabled = false;
+        m_Crouching.enabled = false;
+        m_FPSCamera.enabled = false;
+        m_Interaction.enabled = false;
+
+        Invoke("Revive", deathTime);
+    }
+
+    public void Revive()
+    {
+        canvas.SetActive(true);
+        deathCanvas.SetActive(false);
+        m_Player.enabled = true;
+        m_Bending.enabled = true;
+        m_Crouching.enabled = true;
+        m_FPSCamera.enabled = true;
+        m_Interaction.enabled = true;
     }
 }
